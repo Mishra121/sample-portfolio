e9ad12717a09e71fd42dcde3c704f881ca750a7b
Z0086L0 <Vibhu.Mishra@target.com>
2022-05-18 14:37:21 +0530

    Add navbar component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.css';
+	import Navbar from '$lib/components/navbar.svelte'
 	import Footer from '$lib/components/footer.svelte'
 </script>
 
+<Navbar />
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
