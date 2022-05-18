ddfb01a4046dae5a2adce0c71d27282913f200d6
Z0086L0 <Vibhu.Mishra@target.com>
2022-05-18 14:07:36 +0530

    Add projects index page

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -14,17 +14,34 @@ query GetAuthors {
 }
 `
 
-export const projectsQuery = gql`
-query GetProjects {
-    projects {
-        name
-        slug
-        description
-        demo
-        sourceCode
-        image {
-            url
-        }
+const PROJECT_FRAGMENT = gql`
+  fragment ProjectDetails on Project {
+    name
+    slug
+    description
+    tags
+    demo
+    sourceCode
+    image {
+      url
     }
-}
+  }
+`
+
+export const projectsQuery = gql`
+  ${PROJECT_FRAGMENT}
+  query GetProjects {
+    projects {
+      ...ProjectDetails
+    }
+  }
+`
+
+export const projectQuery = gql`
+  ${PROJECT_FRAGMENT}
+  query GetProject($slug: String!) {
+    project(where: { slug: $slug }) {
+      ...ProjectDetails
+    }
+  }
 `
