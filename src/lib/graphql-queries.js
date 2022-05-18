acb4dc1c8f7015c8cafd0acf8a664c8a67149c6f
Z0086L0 <Vibhu.Mishra@target.com>
2022-05-18 14:20:55 +0530

    Add posts index page and slug page

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -45,3 +45,37 @@ export const projectQuery = gql`
     }
   }
 `
+
+const POST_FRAGMENT = gql`
+  fragment PostDetails on Post {
+    title
+    slug
+    date
+    content
+    tags
+    coverImage {
+      url
+    }
+    authors {
+      name
+    }
+  }
+`
+
+export const postsQuery = gql`
+  ${POST_FRAGMENT}
+  query GetPosts {
+    posts {
+      ...PostDetails
+    }
+  }
+`
+
+export const postQuery = gql`
+  ${POST_FRAGMENT}
+  query GetPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      ...PostDetails
+    }
+  }
+`
